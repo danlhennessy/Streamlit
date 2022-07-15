51a75fb5fcbecb0515702f4b36cbc6d03bf80976
danlhennessy <danh118@hotmail.co.uk>
2022-07-15 19:39:26 +0100

    streamlit themes

--- crypto/app.py
+++ crypto/app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
-import numpy as np
-import matplotlib.pyplot as plt
 
 st.title("Crypto Dashboard")
 
