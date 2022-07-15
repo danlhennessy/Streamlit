0b2c3b8946b64d102553162dbac77834103813be
danlhennessy <danh118@hotmail.co.uk>
2022-07-15 04:05:06 +0100

    revert to panda

--- crypto/app.py
+++ crypto/app.py
@@ -5,14 +5,24 @@ from bs4 import BeautifulSoup as bs
 
 st.title("Crypto Dashboard")
 
-def webtoDF(url):
-    header = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-    }
-    r = requests.get(url, headers=header)
-    html = pd.read_html(r.text)
-    df = html[0] # Set dataframe to be the first table if multiple on page
-    df
+url = "https://crypto.com/price"
+
+# Requests Module gets HTML data
+header = {
+"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+"X-Requested-With": "XMLHttpRequest"
+}
+r = requests.get(url, headers=header)
+
+# BS Scraping:
+
+
+html = pd.read_html(r.text)
+df = html[0] # Set dataframe to be the first table if multiple on page
+df = df.drop(df.columns[[0, 1]], axis=1)
+df
     
-webtoDF("https://coinmarketcap.com/")
+def webtoDF(url):
+    pass
+#webtoDF("https://coinmarketcap.com/")
+
