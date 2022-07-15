07910b513b8f231c04c9047f75e5445cfa8f36b2
danlhennessy <danh118@hotmail.co.uk>
2022-07-15 02:52:17 +0100

    including scraping

--- crypto/app.py
+++ crypto/app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
 
 st.title("Crypto Dashboard")
 
+def webtoDF(url):
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }
+    r = requests.get(url, headers=header)
+    html = pd.read_html(r.text)
+    df = html[0] # Set dataframe to be the first table if multiple on page
+    df
+    
+webtoDF("https://coinmarketcap.com/")
--- st templates/pandaread.py
+++ st templates/pandaread.py
@@ -1,7 +1,14 @@
+import requests
 import pandas as pd
 
-url = "https://en.wikipedia.org/wiki/Forbes%27_list_of_the_world%27s_highest-paid_athletes"
 
-html = pd.read_html(url)
-df = html[0] # Set dataframe to be the first table if multiple on page
-print(df)
+def webtoDF(url):
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }
+    r = requests.get(url, headers=header)
+    html = pd.read_html(r.text)
+    df = html[0] # Set dataframe to be the first table if multiple on page
+    df
+    
