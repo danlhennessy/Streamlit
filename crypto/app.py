1bf8ae5dc532c8b7f58697dff07f88ab9b06abcb
danlhennessy <danh118@hotmail.co.uk>
2022-07-15 05:04:27 +0100

    price column

--- crypto/app.py
+++ crypto/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
+import numpy as np
 
 st.title("Crypto Dashboard")
 
@@ -16,7 +17,7 @@ r = requests.get(url, headers=header)
 
 html = pd.read_html(r.text)
 df = html[0] # Set dataframe to be the first table if multiple on page
-df = df.drop(df.columns[[0,1,2,7,8]], axis=1)
+df = df.drop(df.columns[[0,1,2,3,7,8]], axis=1)
 
 # BS Scraping:
 soup = bs(r.content, "html.parser")
@@ -24,8 +25,18 @@ coinnames = []
 mydivs = soup.find_all("a", {"class": "chakra-text css-o2rp9n"})
 for v in mydivs:
     coinnames.append(v.text)
+prices = []
+initprices = soup.find_all("div", {"class": "css-b1ilzc"})
+for v in initprices:
+    prices.append(v.text)
 
 df.insert(0, "Name", coinnames)
+df.insert(1, "Price", prices)
+
 df
+
+# ToDo: Fix Sorting on each column
+# Add more visuals in streamlit,new tools
+
     
 
--- st templates/pandaDF.py
+++ st templates/pandaDF.py
@@ -3,3 +3,7 @@ df = df.drop(df.columns[[0, 1, 3]], axis=1) # Drop Columns 0, 1 and 3
 df = df.drop(df.columns[[0, 1, 3]], axis=0) # Drop Rows 0, 1 and 3
 
 df = df.drop('column_name', axis=1) # Drop column 'column_name'
+
+df.insert(0, "Name", coinnames) # Insert column at position 0
+
+df.columns.values[0] = "b" # Rename column at position 0
